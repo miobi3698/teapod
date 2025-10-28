72d41466678643e420b1765c411f567df1063850
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2025-10-28 21:34:44 +0700

    added terminal rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{self, Event, KeyEventKind};
+use ratatui::widgets::Paragraph;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut terminal = ratatui::init();
+    'main_loop: loop {
+        terminal.draw(|frame| frame.render_widget(Paragraph::new("Teapod"), frame.area()))?;
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    event::KeyCode::Char('q') => break 'main_loop,
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    }
+
+    ratatui::restore();
+    Ok(())
 }
