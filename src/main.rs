87daa8ff077168c9fce41e3140bb882ff6a057e0
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2025-11-03 09:01:07 +0700

    added example podcast data for ui

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,142 @@
-use std::error::Error;
+use std::{error::Error, fmt::Display};
 
+use chrono::{DateTime, FixedOffset};
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
     layout::{Constraint, Layout},
-    widgets::{Block, Paragraph},
+    style::{Style, Stylize},
+    text::Text,
+    widgets::{Block, Cell, List, Paragraph, Row, Table, TableState},
 };
 
-fn main() -> Result<(), Box<dyn Error>> {
+struct Podcast {
+    title: String,
+    description: String,
+    url: String,
+    episodes: Vec<Episode>,
+}
+
+struct Episode {
+    title: String,
+    description: String,
+    date: DateTime<FixedOffset>,
+    audio: Audio,
+}
+
+struct Audio {
+    mime_type: String,
+    length: usize,
+    url: String,
+}
+
+#[derive(Debug)]
+enum RssParseError {
+    MissingTag,
+    MissingValue,
+    MissingAttr,
+}
+
+impl Display for RssParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl Error for RssParseError {}
+
+async fn download_podcast_info(url: &str) -> Result<Podcast, Box<dyn Error>> {
+    let text = reqwest::get(url).await?.text().await?;
+    let rss = roxmltree::Document::parse(text.as_str())?;
+    let channel = rss
+        .descendants()
+        .find(|elem| elem.has_tag_name("channel"))
+        .ok_or(RssParseError::MissingTag)?;
+    let channel_title = channel
+        .children()
+        .find(|elem| elem.has_tag_name("title"))
+        .ok_or(RssParseError::MissingTag)?
+        .text()
+        .ok_or(RssParseError::MissingValue)?
+        .to_string();
+    let channel_description = channel
+        .children()
+        .find(|elem| elem.has_tag_name("title"))
+        .ok_or(RssParseError::MissingTag)?
+        .text()
+        .ok_or(RssParseError::MissingValue)?
+        .to_string();
+    let episodes: Result<Vec<_>, Box<dyn Error>> = channel
+        .children()
+        .filter(|elem| elem.has_tag_name("item"))
+        .map(|elem| {
+            let episode_title = elem
+                .children()
+                .find(|e| e.has_tag_name("title"))
+                .ok_or(RssParseError::MissingTag)?
+                .text()
+                .ok_or(RssParseError::MissingValue)?
+                .to_string();
+            let episode_description = elem
+                .children()
+                .find(|e| e.has_tag_name("description"))
+                .ok_or(RssParseError::MissingTag)?
+                .text()
+                .ok_or(RssParseError::MissingValue)?
+                .to_string();
+            let episode_date = DateTime::parse_from_rfc2822(
+                elem.children()
+                    .find(|e| e.has_tag_name("pubDate"))
+                    .ok_or(RssParseError::MissingTag)?
+                    .text()
+                    .ok_or(RssParseError::MissingValue)?,
+            )?;
+
+            let episode_audio = {
+                let elem = elem
+                    .children()
+                    .find(|e| e.has_tag_name("enclosure"))
+                    .ok_or(RssParseError::MissingTag)?;
+
+                Audio {
+                    mime_type: elem
+                        .attribute("type")
+                        .ok_or(RssParseError::MissingAttr)?
+                        .to_string(),
+                    length: elem
+                        .attribute("length")
+                        .ok_or(RssParseError::MissingAttr)?
+                        .parse()?,
+                    url: elem
+                        .attribute("url")
+                        .ok_or(RssParseError::MissingAttr)?
+                        .to_string(),
+                }
+            };
+
+            Ok(Episode {
+                title: episode_title,
+                description: episode_description,
+                date: episode_date,
+                audio: episode_audio,
+            })
+        })
+        .collect();
+
+    Ok(Podcast {
+        title: channel_title,
+        description: channel_description,
+        url: url.to_string(),
+        episodes: episodes?,
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let urls = vec!["https://changelog.fm/rss"];
+    let podcasts = vec![download_podcast_info(urls[0]).await?];
+    let selected_podcast = 0;
+    let mut table_state = TableState::default().with_selected(0);
+
     let mut terminal = ratatui::init();
     'main_loop: loop {
         terminal.draw(|frame| {
@@ -22,13 +152,32 @@ fn main() -> Result<(), Box<dyn Error>> {
                     .areas(frame.area());
 
             frame.render_widget(
-                Paragraph::default().block(Block::bordered().title("Podcasts")),
+                List::new(podcasts.iter().enumerate().map(|(index, podcast)| {
+                    if selected_podcast == index {
+                        Text::from(podcast.title.clone()).reversed()
+                    } else {
+                        Text::from(podcast.title.clone())
+                    }
+                }))
+                .block(Block::bordered().title("Podcasts")),
                 podcast_list_area,
             );
 
-            frame.render_widget(
-                Paragraph::default().block(Block::bordered().title("Episodes")),
+            frame.render_stateful_widget(
+                Table::new(
+                    podcasts[selected_podcast].episodes.iter().map(|episode| {
+                        Row::new(vec![
+                            Cell::from(Text::from(episode.title.clone())),
+                            Cell::from(Text::from(episode.date.date_naive().to_string())),
+                        ])
+                    }),
+                    [Constraint::Fill(1), Constraint::Length(10)],
+                )
+                .header(Row::new(vec!["Title", "Date"]).reversed())
+                .row_highlight_style(Style::default().reversed())
+                .block(Block::bordered().title("Episodes")),
                 podcast_episode_list_area,
+                &mut table_state,
             );
         })?;
 
